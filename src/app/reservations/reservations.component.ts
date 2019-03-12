7cf5c960011419a10ef3de8f11333b9942882779
muldoonk <katiem1234@sbcglobal.net>
2019-03-12 06:24:33 -0400

    added some functionality to the reservations page

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-reservations',
@@ -8,11 +9,44 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ReservationsComponent implements OnInit {
   private type: string;
+  //correlates to the seeMore pages
+  private page: number;
+  private machineData: ({ time: string; machines: string[] })[];
+  private timeSlots: string[];
+  private machinesPerPage: number;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.type = this.route.snapshot.paramMap.get("type")
+    this.page = 0;
+    this.machinesPerPage = 4;
+    this.type = this.route.snapshot.paramMap.get("type");
+    this.timeSlots = ['1:00pm', '2:00pm', '3:00pm'];
+    this.machineData = [
+      {time: this.timeSlots[0], machines: ['Tread1', 'Tread2', 'Tread3', 'Tread4', 'Tread5']},
+      {time: this.timeSlots[1], machines: ['Tread1', 'Tread2', 'Tread3', 'Tread4', 'Tread5']},
+      {time: this.timeSlots[2], machines: ['Tread1', 'Tread2', 'Tread3', 'Tread4', 'Tread5']}
+    ]
   }
 
+  public getTimeSlots() {
+    return this.timeSlots;
+  }
+
+  public nextPage() {
+    this.page++;
+  }
+
+  public getMachinesByTime(givenTime) {
+    const startIndex = this.page * this.machinesPerPage;
+    return _.get(_.find(this.machineData, (data) => data.time === givenTime), 'machines').slice(startIndex, startIndex + 4);
+  }
+
+  public reserve(name, time) {
+    alert("Booking successful for " + name + " at " + time);
+  }
+
+  public lastPage() {
+    this.page--;
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,9 +6,23 @@ import {Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  private open: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.open = false;
+  }
+
+  public toggleNav() {
+    if (!this.open) {
+      document.getElementById("mySidebar").style.width = "275px";
+      document.getElementById("main").style.marginLeft = "275px";
+      this.open = true;
+    } else {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+      this.open = false;
+    }
   }
 }
