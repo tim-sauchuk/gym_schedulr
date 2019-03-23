4fbcaa4b8e0e034d3426f774b2070093a53e797b
muldoonk <katiem1234@sbcglobal.net>
2019-03-23 14:46:50 -0400

    added remove functionality to routines page

--- src/app/routines/routines.component.ts
+++ src/app/routines/routines.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-routines',
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoutinesComponent implements OnInit {
 	public _date: string;
+  public reservedMachines: any[];
 
   constructor() { }
 
   ngOnInit() {
     this._date = new Date().toISOString().slice(0, 10);
+    this.reservedMachines = [
+      {name: "Tread1", time: "7:30-8:30 AM"},
+      {name: "Squat3", time: "8:00-8:10 AM"},
+      {name: "Elliptical6", time:"8:10-8:30 AM"}
+    ];
   }
 
+  remove(machine) {
+    _.remove(this.reservedMachines, (reservedMachine) => reservedMachine === machine);
+  }
 }
