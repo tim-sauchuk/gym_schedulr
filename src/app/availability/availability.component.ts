0754d70ef13450e620b4d1a65237167b1fce77a8
Rachel <prendergass.r@husky.neu.edu>
2019-03-26 11:43:22 -0400

    updating availability chart

--- src/app/availability/availability.component.ts
+++ src/app/availability/availability.component.ts
@@ -8,44 +8,24 @@ declare let google: any;
 })
 export class AvailabilityComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
+
 
   ngOnInit() {
     google.charts.setOnLoadCallback(this.drawChart);
   }
 
   drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['Time', 'Percentage of Machines in Use'],
-      ['5AM',  20],
-      ['6AM',  30],
-      ['7AM',  60],
-      ['8AM',  60],
-      ['9AM',  45],
-      ['10AM', 45],
-      ['11AM', 40],
-      ['12PM', 30],
-      ['1PM', 35],
-      ['2PM', 45],
-      ['3PM', 50],
-      ['4PM', 55],
-      ['5PM', 55],
-      ['6PM', 70],
-      ['7PM', 100],
-      ['8PM', 80],
-      ['9PM', 70],
-      ['10PM', 55],
-      ['11PM', 40],
-      ['12AM', 25],
-      ['1AM', 10]
-    ]);
+    var d = document.getElementById("machine_dropdown") as HTMLSelectElement;
+    var machineType = d.options[d.selectedIndex].text;
 
-    var view = new google.visualization.DataView(data);
-    document.getElementById('machine_type').onclick=function() {
+    if (machineType == 'All Machines') {
       var data = google.visualization.arrayToDataTable([
-        ['Time', 'Percentage of Machines in Use'],
+        ['Time', 'Percentage of ' + machineType + ' in Use'],
         ['5AM',  20],
-        ['6AM',  100],
+        ['6AM',  30],
         ['7AM',  60],
         ['8AM',  60],
         ['9AM',  45],
@@ -53,12 +33,12 @@ export class AvailabilityComponent implements OnInit {
         ['11AM', 40],
         ['12PM', 30],
         ['1PM', 35],
-        ['2PM', 25],
+        ['2PM', 45],
         ['3PM', 50],
         ['4PM', 55],
-        ['5PM', 0],
+        ['5PM', 55],
         ['6PM', 70],
-        ['7PM', 90],
+        ['7PM', 100],
         ['8PM', 80],
         ['9PM', 70],
         ['10PM', 55],
@@ -66,16 +46,90 @@ export class AvailabilityComponent implements OnInit {
         ['12AM', 25],
         ['1AM', 10]
       ]);
-      var options = {
-        title: '',
-        bar: {groupWidth: "97%"},
-        legend: { position: "none" },
-      };
-      var view = new google.visualization.DataView(data);
-      chart.draw(view, options);
     }
+    else if (machineType == 'Treadmills') {
+      var data = google.visualization.arrayToDataTable([
+        ['Time', 'Percentage of ' + machineType + ' in Use'],
+        ['5AM',  30],
+        ['6AM',  90],
+        ['7AM',  80],
+        ['8AM',  60],
+        ['9AM',  50],
+        ['10AM', 50],
+        ['11AM', 40],
+        ['12PM', 30],
+        ['1PM', 35],
+        ['2PM', 45],
+        ['3PM', 50],
+        ['4PM', 55],
+        ['5PM', 55],
+        ['6PM', 70],
+        ['7PM', 90],
+        ['8PM', 100],
+        ['9PM', 100],
+        ['10PM', 55],
+        ['11PM', 40],
+        ['12AM', 25],
+        ['1AM', 40]
+      ]);
+    }
+    else if (machineType == 'Squat Racks') {
+      var data = google.visualization.arrayToDataTable([
+        ['Time', 'Percentage of ' + machineType + ' in Use'],
+        ['5AM',  0],
+        ['6AM',  10],
+        ['7AM',  40],
+        ['8AM',  40],
+        ['9AM',  30],
+        ['10AM', 40],
+        ['11AM', 40],
+        ['12PM', 30],
+        ['1PM', 60],
+        ['2PM', 60],
+        ['3PM', 70],
+        ['4PM', 70],
+        ['5PM', 55],
+        ['6PM', 75],
+        ['7PM', 100],
+        ['8PM', 100],
+        ['9PM', 100],
+        ['10PM', 70],
+        ['11PM', 40],
+        ['12AM', 25],
+        ['1AM', 20]
+      ]);
+    }
+    else if (machineType == 'Stairmasters') {
+      var data = google.visualization.arrayToDataTable([
+        ['Time', 'Percentage of ' + machineType + ' in Use'],
+        ['5AM',  20],
+        ['6AM',  50],
+        ['7AM',  70],
+        ['8AM',  70],
+        ['9AM',  50],
+        ['10AM', 45],
+        ['11AM', 40],
+        ['12PM', 30],
+        ['1PM', 50],
+        ['2PM', 60],
+        ['3PM', 60],
+        ['4PM', 55],
+        ['5PM', 55],
+        ['6PM', 70],
+        ['7PM', 100],
+        ['8PM', 90],
+        ['9PM', 100],
+        ['10PM', 55],
+        ['11PM', 40],
+        ['12AM', 35],
+        ['1AM', 30]
+      ]);
+    }
+
+    var view = new google.visualization.DataView(data);
+
     var options = {
-      title: 'Percentage of Machines in Use',
+      title: 'Percentage of ' + machineType + ' in Use',
       bar: {groupWidth: "97%"},
       legend: { position: "none" },
     };
